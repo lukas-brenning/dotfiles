bbfaff75f1721c100ee3b595af770c8c48854c23
Lukas Brenning <Lukas_Brenning-COL@McKinsey.com>
2019-05-11 15:49:40 +0200

    Initial commit

---
+++ first_setup.sh
@@ -0,0 +1,56 @@
+# Homebrew
+# ------------------------------------------
+# Install X-Code
+xcode-select --install
+
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Check installation
+brew doctor
+
+# Install applications
+# ------------------------------------------
+# Atom
+brew cask install atom
+
+# Caffeine
+brew cask install caffeine
+
+# CheatSheet
+brew cask install cheatsheet
+
+# Docker
+
+# Duet
+brew cask install duet
+
+# JetBrains Toolbox
+brew cask install jetbrains-toolbox
+
+# Jumpcut
+brew cask install jumpcut
+
+# Skitch
+brew cask install skitch
+
+# Paintbrush
+brew cask install paintbrush
+
+# R
+brew install r
+
+# RStudio
+brew cask install rstudio
+
+# Slack
+brew cask install slack
+
+# Spotify
+brew cask install spotify
+
+# Telegram
+brew cask install telegram
+
+# WhatsApp
+brew cask install whatsapp
