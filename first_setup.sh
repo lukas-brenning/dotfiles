ef693fff1799ece2d6d170b2960f9fadcdc18205
Lukas Brenning <Lukas_Brenning-COL@McKinsey.com>
2019-05-11 16:00:18 +0200

    adding docker installation

--- first_setup.sh
+++ first_setup.sh
@@ -7,7 +7,18 @@ xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Check installation
-brew doctor
+brew install docker
+brew install docker-machine
+brew services start docker-machine
+brew install virtualbox  # Requires SystemPreferences intervention
+docker-machine create default --virtualbox-cpu-count 6 --virtualbox-memory 8192
+
+# Create "docker" group and put me in it
+sudo dseditgroup -o create docker
+sudo dseditgroup -o edit -a price -t user docker
+
+# Run this to set environment before running docker
+eval $(docker-machine env default)
 
 # Install applications
 # ------------------------------------------
